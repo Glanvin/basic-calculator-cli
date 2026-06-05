9e980c325eb6cf023b668fd2975eee8ae71ff554
Glanvin <glavin.code@gmail.com>
2026-06-06 02:07:01 +0800

    Basic Calculation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
-use std::io;
+use std::{io};
 
 fn main() {
-    println!("Hello, world!\n");
+    println!("Hello, Welcome to Basic Calculator!");
+    println!("Here put your numbers separated by commas(e.g. 1,2,3):");
+    calculate();
+}
 
-    let ivy = "Ivy";
-    let ivy_age = 24;
-    println!("Hello, I'm {} and I'm {} years old!", ivy, ivy_age);
-
-    let nyo = "Nyo";
-    let nyo_age = 23;
-    println!("\nHello, I'm {} and I'm {} years old!", nyo, nyo_age);
-
-    println!("What's your name?\nEnter your name: ");
-
-    let mut user = String::new();
-    io::stdin().read_line(&mut user).unwrap_or_default();
-    println!("Hello, {}!", user.trim());
+fn calculate() {
+    let mut numbers = Vec::new();
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap_or_default();
+    let inputs: Vec<&str> = inputs.trim().split(',').collect();
+    for input in inputs {
+        if let Ok(num) = input.trim().parse::<i64>() {
+            numbers.push(num);
+        }
+    }
+    println!("Your numbers are {:?}", numbers);
+    println!("Choose an operation +, /, -, %");
+    let mut operation = String::new();
+    io::stdin().read_line(&mut operation).unwrap_or_default();
+    let operation = operation.trim();
+    let result = match operation {
+        "+" => numbers.iter().sum::<i64>(),
+        "-" => numbers.iter().fold(0, |acc, &num| acc - num),
+        "*" => numbers.iter().fold(1, |acc, &num| acc * num),
+        "/" => numbers.iter().fold(1, |acc, &num| acc / num),
+        _ => {
+            println!("Invalid operation");
+            return;
+        }
+    };
+    println!("Result: {}", result);
 }
